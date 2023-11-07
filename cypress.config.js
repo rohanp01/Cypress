04b0494265f9987674ef52f934735a96443eee7b
rohanp01 <rohanp3003@gmail.com>
2023-11-07 12:54:57 +0530

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+  env: {
+
+    url: 'https://rahulshettyacademy.com/angularpractice/'
+  },
+
+
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern:'cypress/integration/samples/*.cy.js'
+    //specPattern:'cypress/integration/PageObject/*.cy.js'
+  }
+
+
+
+});
+
+
+
+
+
+
+
+
+
